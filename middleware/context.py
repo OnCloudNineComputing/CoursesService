0ff76baecf395cd35171d45521926a24b520f016
Srujan Esanakarra <47041673+srujan7@users.noreply.github.com>
2021-10-16 23:36:43 -0400

    Changed context so database info must be configured as environment variables

--- middleware/context.py
+++ middleware/context.py
@@ -10,19 +10,11 @@ def get_db_info():
     db_user = os.environ.get("DBUSER", None)
     db_password = os.environ.get("DBPASSWORD", None)
 
-    if db_host and db_user and db_password:
-        db_info = {
-            "host": db_host,
-            "user": db_user,
-            "password": db_password,
-            "cursorclass": pymysql.cursors.DictCursor
-        }
-    else:
-        db_info = {
-            "host": "localhost",
-            "user": "mysqluser",
-            "password": "mysqluserve2170",
-            "cursorclass": pymysql.cursors.DictCursor
+    db_info = {
+        "host": db_host,
+        "user": db_user,
+        "password": db_password,
+        "cursorclass": pymysql.cursors.DictCursor
         }
 
     return db_info
