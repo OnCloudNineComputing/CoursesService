11bbcf483a2df24eff84d941a4d1f783edd6d666
Srujan <47041673+srujan7@users.noreply.github.com>
2021-12-06 00:00:35 -0500

    Refactored service to separate TAs and professors into individual tables to allow more than one TA and/or professor per course

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ import utils.rest_utils as rest_utils
 
 from application_services.CoursesResource.courses_service \
     import CoursesResource
+from application_services.CoursesResource.ta_course_service import \
+    TACourseResource
+from application_services.CoursesResource.professor_course_service import \
+    ProfessorCourseResource
 from integrity_services.CoursesIntegrityResource import CoursesIntegrity
-from database_services.CoursesRDBService import CoursesRDBService
+from integrity_services.TACourseIntegrityResource import TACourseIntegrity
+from integrity_services.ProfessorCourseIntegrityResource import \
+    ProfessorCourseIntegrity
+from database_services.RDBServices.CoursesRDBService import CoursesRDBService
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
@@ -267,6 +274,338 @@ def course_by_id(course_id):
     except Exception as e:
         # HTTP status code.
         print("/api/courses/" + str(course_id) + ", e = ", str(e))
+        rsp = Response("INTERNAL ERROR", status=500,
+                       content_type="text/plain")
+
+    return rsp
+
+
+@app.route('/api/ta', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def ta_course():
+    """
+    1. HTTP GET return all TAs for all courses.
+    2. HTTP POST with body --> add a TA to a course
+    3. HTTP PUT update all TA records that match the provided where clause
+    with the updated data
+    --> JSON must be nested as follows
+    {
+        "update_fields": {
+            "field1": "value",
+            "field2": "value",
+            ...
+            "last_field": "value"
+        },
+        "where_fields": {
+            "field1": "value",
+            "field2": "value",
+            ...
+            "last_field": "value"
+        }
+    4. HTTP DELETE delete all TAs for all courses
+    :return: response from desired action
+    """
+    try:
+        inputs = rest_utils.RESTContext(request)
+        rest_utils.log_request("ta_course", inputs)
+
+        if inputs.method == "GET":
+            limit = 5
+            if inputs.limit:
+                if int(inputs.limit) < limit:
+                    limit = int(inputs.limit)
+            offset = 0
+            if inputs.offset:
+                offset = int(inputs.offset)
+            order_by = inputs.order_by
+            res = TACourseResource.get_by_template(inputs.args,
+                                                   order_by=order_by,
+                                                   limit=limit,
+                                                   offset=offset,
+                                                   field_list=inputs.fields)
+            if res:
+                res = TACourseResource.add_course_links(res)
+                res = TACourseResource.get_links(res, inputs)
+            rsp = TACourseIntegrity.ta_get_responses(res)
+        elif inputs.method == "POST":
+            validation = TACourseIntegrity.input_validation(inputs.data)
+            if validation[0] == 200:
+                res = TACourseResource.create(inputs.data)
+                if res == 1:
+                    res = inputs.data["uni"]
+            else:
+                res = validation
+            rsp = TACourseIntegrity.post_responses(res)
+        elif inputs.method == "PUT":
+            data_template = inputs.data['update_fields']
+            where_template = inputs.data['where_fields']
+            update_fields_val = TACourseIntegrity.type_validation(data_template)
+            where_fields_val = TACourseIntegrity.type_validation(where_template)
+            if update_fields_val[0] == 200 and where_fields_val[0] == 200:
+                res = TACourseResource.update_by_template(data_template,
+                                                          where_template)
+            else:
+                error_code = 0
+                errors = {}
+                if update_fields_val[0] != 200:
+                    error_code = update_fields_val[0]
+                    errors["Errors in update_fields"] = update_fields_val[1]
+                if where_fields_val[0] != 200:
+                    error_code = where_fields_val[0]
+                    errors["Errors in where_fields"] = where_fields_val[1]
+                res = (error_code, errors)
+            rsp = TACourseIntegrity.ta_put_responses(res)
+        elif inputs.method == "DELETE":
+            res = TACourseResource.delete_by_template(inputs.args)
+            rsp = TACourseIntegrity.ta_delete_responses(res)
+        else:
+            rsp = Response("NOT IMPLEMENTED", status=501)
+
+    except Exception as e:
+        # HTTP status code.
+        print("/api/ta, e = ", str(e))
+        rsp = Response("INTERNAL ERROR", status=500,
+                       content_type="text/plain")
+
+    return rsp
+
+
+@app.route('/api/ta/<uni>', methods=['GET', 'PUT', 'DELETE'])
+def ta_course_by_uni(uni):
+    """
+    1. HTTP GET return matching records for the given TA by UNI.
+    2. HTTP PUT match records by TA UNI and update specific fields
+    --> JSON must be nested as follows for update
+    {
+        "field1": "value",
+        "field2": "value",
+        ...
+        "last_field": "value"
+    }
+    3. HTTP DELETE match records by TA UNI and delete them
+    :param uni: TA's UNI
+    :return: response from desired action
+    """
+    try:
+        uni_validation = TACourseIntegrity.type_validation({"uni": uni})
+        if uni_validation[0] == 400:
+            raise ValueError(uni_validation[1])
+
+        inputs = rest_utils.RESTContext(request)
+        rest_utils.log_request("ta_course_by_uni", inputs)
+
+        if inputs.method == "GET":
+            limit = 5
+            if inputs.limit:
+                if int(inputs.limit) < limit:
+                    limit = int(inputs.limit)
+            offset = 0
+            if inputs.offset:
+                offset = int(inputs.offset)
+            order_by = inputs.order_by
+            res = TACourseResource.get_by_uni(uni, order_by=order_by,
+                                              limit=limit, offset=offset,
+                                              field_list=inputs.fields)
+            if res:
+                res = TACourseResource.add_course_links(res)
+                res = TACourseResource.get_links(res, inputs)
+            rsp = TACourseIntegrity.ta_by_uni_get_responses(res)
+        elif inputs.method == "PUT":
+            validation = TACourseIntegrity.type_validation(inputs.data)
+            if validation[0] == 200:
+                res = TACourseResource.update_by_uni(uni, inputs.data)
+            else:
+                res = validation
+            rsp = TACourseIntegrity.ta_by_uni_put_responses(res, uni)
+        elif inputs.method == "DELETE":
+            test = TACourseResource.get_by_uni(uni)
+            if test:
+                res = TACourseResource.delete_by_uni(uni)
+            else:
+                res = None
+            rsp = TACourseIntegrity.ta_by_uni_delete_responses(res, uni)
+        else:
+            rsp = Response("NOT IMPLEMENTED", status=501)
+
+    except ValueError as v:
+        print("/api/ta/" + str(uni))
+        if type(v) == str:
+            rsp = Response(str(v), status=400, content_type="text/plain")
+        else:
+            rsp = Response(json.dumps(v, default=str), status=400,
+                           content_type="application/json")
+
+    except Exception as e:
+        # HTTP status code.
+        print("/api/ta/" + str(uni) + ", e = ", str(e))
+        rsp = Response("INTERNAL ERROR", status=500,
+                       content_type="text/plain")
+
+    return rsp
+
+
+@app.route('/api/professor', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def professor_course():
+    """
+    1. HTTP GET return all professors for all courses.
+    2. HTTP POST with body --> add a professor to a course
+    3. HTTP PUT update all professor records that match the provided where
+    clause with the updated data
+    --> JSON must be nested as follows
+    {
+        "update_fields": {
+            "field1": "value",
+            "field2": "value",
+            ...
+            "last_field": "value"
+        },
+        "where_fields": {
+            "field1": "value",
+            "field2": "value",
+            ...
+            "last_field": "value"
+        }
+    4. HTTP DELETE delete all professors for all courses
+    :return: response from desired action
+    """
+    try:
+        inputs = rest_utils.RESTContext(request)
+        rest_utils.log_request("professor_course", inputs)
+
+        if inputs.method == "GET":
+            limit = 5
+            if inputs.limit:
+                if int(inputs.limit) < limit:
+                    limit = int(inputs.limit)
+            offset = 0
+            if inputs.offset:
+                offset = int(inputs.offset)
+            order_by = inputs.order_by
+            res = ProfessorCourseResource.get_by_template(inputs.args,
+                                                          order_by=order_by,
+                                                          limit=limit,
+                                                          offset=offset,
+                                                          field_list=
+                                                          inputs.fields)
+            if res:
+                res = ProfessorCourseResource.add_course_links(res)
+                res = ProfessorCourseResource.get_links(res, inputs)
+            rsp = ProfessorCourseIntegrity.professor_get_responses(res)
+        elif inputs.method == "POST":
+            validation = ProfessorCourseIntegrity.input_validation(inputs.data)
+            if validation[0] == 200:
+                res = ProfessorCourseResource.create(inputs.data)
+                if res == 1:
+                    res = inputs.data["uni"]
+            else:
+                res = validation
+            rsp = ProfessorCourseIntegrity.post_responses(res)
+        elif inputs.method == "PUT":
+            data_template = inputs.data['update_fields']
+            where_template = inputs.data['where_fields']
+            update_fields_val = ProfessorCourseIntegrity.type_validation(
+                data_template)
+            where_fields_val = ProfessorCourseIntegrity.type_validation(
+                where_template)
+            if update_fields_val[0] == 200 and where_fields_val[0] == 200:
+                res = ProfessorCourseResource.update_by_template(
+                    data_template, where_template)
+            else:
+                error_code = 0
+                errors = {}
+                if update_fields_val[0] != 200:
+                    error_code = update_fields_val[0]
+                    errors["Errors in update_fields"] = update_fields_val[1]
+                if where_fields_val[0] != 200:
+                    error_code = where_fields_val[0]
+                    errors["Errors in where_fields"] = where_fields_val[1]
+                res = (error_code, errors)
+            rsp = ProfessorCourseIntegrity.professor_put_responses(res)
+        elif inputs.method == "DELETE":
+            res = ProfessorCourseResource.delete_by_template(inputs.args)
+            rsp = ProfessorCourseIntegrity.professor_delete_responses(res)
+        else:
+            rsp = Response("NOT IMPLEMENTED", status=501)
+
+    except Exception as e:
+        # HTTP status code.
+        print("/api/professor, e = ", str(e))
+        rsp = Response("INTERNAL ERROR", status=500,
+                       content_type="text/plain")
+
+    return rsp
+
+
+@app.route('/api/professor/<uni>', methods=['GET', 'PUT', 'DELETE'])
+def professor_course_by_uni(uni):
+    """
+    1. HTTP GET return matching records for the given professor by UNI.
+    2. HTTP PUT match records by professor UNI and update specific fields
+    --> JSON must be nested as follows for update
+    {
+        "field1": "value",
+        "field2": "value",
+        ...
+        "last_field": "value"
+    }
+    3. HTTP DELETE match records by professor UNI and delete them
+    :param uni: professor's UNI
+    :return: response from desired action
+    """
+    try:
+        uni_validation = ProfessorCourseIntegrity.type_validation({"uni": uni})
+        if uni_validation[0] == 400:
+            raise ValueError(uni_validation[1])
+
+        inputs = rest_utils.RESTContext(request)
+        rest_utils.log_request("professor_course_by_uni", inputs)
+
+        if inputs.method == "GET":
+            limit = 5
+            if inputs.limit:
+                if int(inputs.limit) < limit:
+                    limit = int(inputs.limit)
+            offset = 0
+            if inputs.offset:
+                offset = int(inputs.offset)
+            order_by = inputs.order_by
+            res = ProfessorCourseResource.get_by_uni(uni, order_by=order_by,
+                                                     limit=limit,
+                                                     offset=offset,
+                                                     field_list=inputs.fields)
+            if res:
+                res = ProfessorCourseResource.add_course_links(res)
+                res = ProfessorCourseResource.get_links(res, inputs)
+            rsp = ProfessorCourseIntegrity.professor_by_uni_get_responses(res)
+        elif inputs.method == "PUT":
+            validation = ProfessorCourseIntegrity.type_validation(inputs.data)
+            if validation[0] == 200:
+                res = ProfessorCourseResource.update_by_uni(uni, inputs.data)
+            else:
+                res = validation
+            rsp = ProfessorCourseIntegrity.professor_by_uni_put_responses(
+                res, uni)
+        elif inputs.method == "DELETE":
+            test = ProfessorCourseResource.get_by_uni(uni)
+            if test:
+                res = ProfessorCourseResource.delete_by_uni(uni)
+            else:
+                res = None
+            rsp = ProfessorCourseIntegrity.professor_by_uni_delete_responses(
+                res, uni)
+        else:
+            rsp = Response("NOT IMPLEMENTED", status=501)
+
+    except ValueError as v:
+        print("/api/professor/" + str(uni))
+        if type(v) == str:
+            rsp = Response(str(v), status=400, content_type="text/plain")
+        else:
+            rsp = Response(json.dumps(v, default=str), status=400,
+                           content_type="application/json")
+
+    except Exception as e:
+        # HTTP status code.
+        print("/api/professor/" + str(uni) + ", e = ", str(e))
         rsp = Response("INTERNAL ERROR", status=500,
                        content_type="text/plain")
 
---
+++ database_services/RDBServices/ProfessorCourseRDBService.py
@@ -0,0 +1,100 @@
+from database_services.RDBService import RDBService
+
+
+class ProfessorCourseRDBService(RDBService):
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def find_by_uni(cls, db_schema, table_name, uni, order_by=None,
+                    limit=None, offset=None, field_list=None):
+
+        template = {"uni": uni}
+
+        return cls.find_by_template(db_schema, table_name, template,
+                                    order_by, limit, offset, field_list)
+
+    @classmethod
+    def delete_by_uni(cls, db_schema, table_name, uni):
+
+        template = {"uni": uni}
+
+        return cls.delete_by_template(db_schema, table_name, template)
+
+    @classmethod
+    def update_by_uni(cls, db_schema, table_name, uni, data):
+
+        template = {"uni": uni}
+
+        return cls.update_by_template(db_schema, table_name, data, template)
+
+    @classmethod
+    def find_by_professor_name(cls, db_schema, table_name, name,
+                               order_by=None, limit=None, offset=None,
+                               field_list=None):
+
+        template = {"professor": name}
+
+        return cls.find_by_template(db_schema, table_name, template,
+                                    order_by, limit, offset, field_list)
+
+    @classmethod
+    def delete_by_professor_name(cls, db_schema, table_name, name):
+
+        template = {"professor": name}
+
+        return cls.delete_by_template(db_schema, table_name, template)
+
+    @classmethod
+    def update_by_professor_name(cls, db_schema, table_name, name, data):
+
+        template = {"professor": name}
+
+        return cls.update_by_template(db_schema, table_name, data, template)
+
+    @classmethod
+    def find_by_course_id(cls, db_schema, table_name, course_id,
+                          order_by=None, limit=None, offset=None,
+                          field_list=None):
+
+        template = {"course_id": course_id}
+
+        return cls.find_by_template(db_schema, table_name, template,
+                                    order_by, limit, offset, field_list)
+
+    @classmethod
+    def delete_by_course_id(cls, db_schema, table_name, course_id):
+
+        template = {"course_id": course_id}
+
+        return cls.delete_by_template(db_schema, table_name, template)
+
+    @classmethod
+    def update_by_course_id(cls, db_schema, table_name, course_id, data):
+
+        template = {"course_id": course_id}
+
+        return cls.update_by_template(db_schema, table_name, data, template)
+
+    @classmethod
+    def create(cls, db_schema, table_name, create_data):
+        cols = []
+        vals = []
+        args = []
+
+        for k, v in create_data.items():
+            cols.append(k)
+            vals.append('%s')
+            args.append(v)
+
+        cols_clause = "(" + ",".join(cols) + ")"
+        vals_clause = "VALUES (" + ",".join(vals) + ")"
+
+        sql_stmt = "INSERT INTO " + db_schema + "." + table_name + " " + \
+                   cols_clause + " " + vals_clause
+
+        res = cls.run_sql(sql_stmt, args)
+
+        return res
+
